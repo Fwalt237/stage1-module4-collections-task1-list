89613a0c4cdff1ce8162793e5cea0c4befe86ef7
Fwalt237 <patrickkamwa17@gmail.com>
2025-04-01 22:51:58 +0100

    List

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -2,8 +2,29 @@ package com.epam.mjc.collections.list;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> evenNumbers = new LinkedList<>();
+        LinkedList<Integer> oddNumbers = new LinkedList<>();
+
+        for (Integer number : sourceList) {
+            if (number % 2 == 0) {
+                evenNumbers.add(number);
+            } else {
+                oddNumbers.add(number);
+            }
+        }
+        LinkedList<Integer> reverseOddNumbers = new LinkedList<>();
+        ListIterator<Integer> iterator = oddNumbers.listIterator(oddNumbers.size());
+        while (iterator.hasPrevious()) {
+            int number = iterator.previous();
+            reverseOddNumbers.add(number);
+        }
+        reverseOddNumbers.addAll(evenNumbers);
+        return reverseOddNumbers;
+
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,25 @@
 package com.epam.mjc.collections.list;
 
+
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        Comparator<String> comparator = new ListComparator() ;
+        sourceList.sort(comparator);
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        try{
+            int first = Integer.parseInt(a);
+            int second = Integer.parseInt(b);
+            return ((first*5)^2+3) - ((second*5)^2+3);
+        } catch (NumberFormatException e){
+            return 0;
+        }
     }
 }
